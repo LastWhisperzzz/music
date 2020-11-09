dfca2e185017fdf985e9f50e57df4168ffdbf2c8
Last Whisper <xxzworkmail@gmail.com>
2020-11-09 22:33:22 +0800

    修改url

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ let api_base_url = ''
 if (process.env.NODE_ENV === 'development') {
   api_base_url = 'http://localhost:3000'
 } else if (process.env.NODE_ENV === 'production') {
-  api_base_url = 'http://music-api.lastwhisper.net/'
+  api_base_url = 'http://lastwhisper.net:3000/'
 }
 
 export default { api_base_url }
